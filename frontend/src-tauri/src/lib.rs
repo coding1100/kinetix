3bdb8be96cb65c9f5aacea6fccc359d32d6c5f9e
coding1100 <coding1343453@gmail.com>
2026-09-02 18:39:59 +0500

    feat(android): initialize tauri v2 android target & build APK

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,21 +1,23 @@
-use tauri::Manager;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+        .plugin(tauri_plugin_updater::Builder::new().build());
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let _ = app.get_webview_window("main").map(|window| {
                 let _ = window.show();
                 let _ = window.unminimize();
                 let _ = window.set_focus();
             });
-        }))
-        .setup(|_app| {
-            Ok(())
-        })
+        }));
+    }
+
+    builder
+        .setup(|_app| Ok(()))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
